6823f2f74298794bbc1704122dd37183f5cbf977
OskarKaminski <priv.oskar@gmail.com>
2017-04-16 12:39:32 +0200

    Current user, userById and users API

--- src/api/api.router.js
+++ src/api/api.router.js
@@ -1,10 +1,12 @@
 import router from 'koa-router';
 import * as feedback from './feedback/feedback';
-import {getUserById} from './user/user';
+import {getUserById, getUsers, getUserByAuth} from './user/user';
 
 export const api = router();
 
 api.get('/feedback/:id', feedback.getReceived)
 api.put('/feedback/:id/accept', feedback.accept)
 api.put('/feedback/:id/reject', feedback.reject)
+api.get('/current-user/:id', getUserByAuth)
 api.get('/user/:id', getUserById)
+api.get('/users/:groupId', getUsers)
--- src/api/user/user.js
+++ src/api/user/user.js
@@ -1,4 +1,5 @@
 import {User} from '../../db/models/User';
+import {db} from '../../db';
 
 function getUser (authId) {
     return User.findOne({
@@ -12,6 +13,25 @@ export const getReceivedFeedback = (ctx) => {
     ctx.body = '';
 };
 
+export async function getUserByAuth (ctx) {
+    return await db('users')
+        .where('authId', ctx.params.id)
+        .then(data => {
+            ctx.body = data[0];
+        })
+};
+
 export async function getUserById (ctx) {
-    ctx.body = await getUser(ctx.params.id)
+    return await db('users')
+        .where('id', ctx.params.id)
+        .then(data => {
+            ctx.body = data;
+        })
+};
+
+export async function getUsers (ctx) {
+    return await db('users')
+        .then(data => {
+            ctx.body = data;
+        })
 };
